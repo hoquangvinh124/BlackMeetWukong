2a2022e206794db997b97ba102b6dfe95a569321
Vinh Ho <hoquangvinh124@gmail.com>
2024-09-26 01:04:21 +0700

    Tai nguyen

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from MainUI import Ui_MainWindow
+from MainUI import Ui_MainWindow  # Giao diện được tạo từ Qt Designer
+import resources# Tệp chứa tài nguyên từ .qrc
 
 app = QApplication([])
 
-# Tạo đối tượng QMainWindow
+# Tạo cửa sổ QMainWindow
 mainWindow = QMainWindow()
 
-# Tạo đối tượng giao diện Ui_MainWindow
+# Tạo đối tượng giao diện từ Ui_MainWindow
 ui = Ui_MainWindow()
 
-# Thiết lập giao diện cho QMainWindow
+# Thiết lập giao diện cho cửa sổ chính
 ui.setupUi(mainWindow)
 
-# Hiển thị cửa sổ chính
+# Hiển thị cửa sổ
 mainWindow.show()
 
 # Chạy ứng dụng
