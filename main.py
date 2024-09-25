f7db06b0f8189e862b9a11e6a0fb15f8f7a5a449
Vinh Ho <hoquangvinh124@gmail.com>
2024-09-26 00:03:09 +0700

    Chạy app

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
-
 from MainUI import Ui_MainWindow
 
-app=QApplication([])
-myWindow=Ui_MainWindow()
-myWindow.setupUi(QMainWindow())
-myWindow.show()
+app = QApplication([])
+
+# Tạo đối tượng QMainWindow
+mainWindow = QMainWindow()
+
+# Tạo đối tượng giao diện Ui_MainWindow
+ui = Ui_MainWindow()
+
+# Thiết lập giao diện cho QMainWindow
+ui.setupUi(mainWindow)
+
+# Hiển thị cửa sổ chính
+mainWindow.show()
+
+# Chạy ứng dụng
 app.exec()
