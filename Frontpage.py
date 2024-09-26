692438aa9d6ae60eb2beb9df6b1f1966cb3cba10
Vinh Ho <hoquangvinh124@gmail.com>
2024-09-26 22:36:02 +0700

    Fix Ui

--- Frontpage.py
+++ Frontpage.py
@@ -4,7 +4,7 @@ from MainUI import Ui_MainWindow
 
 class MySideBar(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super().__init()
+        super().__init__()
         self.setupUi(self)
         self.setWindowTitle('SideBar Menu')
 
