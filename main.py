592db3271d8e0fe660a6ea8f6f3cfea10daef3ec
Vinh Ho <hoquangvinh124@gmail.com>
2024-09-26 22:27:08 +0700

    Fix Ui

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
 from MainUI import Ui_MainWindow
 import resources_rc
+from Frontpage import  MySideBar
 
-app = QApplication([])  # Initialize the application
-MainWindow = QMainWindow()  # Create an instance of QMainWindow
-ui = Ui_MainWindow()  # Create an instance of the UI class
-ui.setupUi(MainWindow)  # Set up the UI on the QMainWindow instance
-MainWindow.show()  # Show the QMainWindow
-app.exec()  # Execute the application
+
+app = QApplication(sys.argv)
+window=MySideBar()
+window.show()
+app.exec()
